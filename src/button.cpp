1ca075a3766cfbb77d287839f732e9f98b70d237
itssme <itssme3000@gmail.com>
2019-04-05 11:13:30 +0200

    added first version of package

---
+++ src/button.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "ros/ros.h"
+#include "button/push_button.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "ros_button");
+    ros::NodeHandle n;
+    ros::Publisher button_pub = n.advertise<button::push_button>("button", 1000);
+
+    ros::Rate loop_rate(10);
+    while (ros::ok()) {
+        button::push_button button_pressed;
+        button_pressed.pressed = true;
+        std::cin.ignore();
+        button_pub.publish(button_pressed);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
